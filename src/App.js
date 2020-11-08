c5f7a8f6bbbef09a5b4ec41bf34224461ed74925
Justin Banusing <hello@justinbanusing.com>
2020-11-09 02:00:20 +0800

    added stuff

--- src/App.js
+++ src/App.js
@@ -123,4 +123,68 @@ class App extends React.Component {
   }
 }
 
+export class HeaderNav extends React.Component {
+  render() {
+    return (
+      <Navbar bg="light">
+        <Navbar.Brand>E-Gacha</Navbar.Brand>
+      </Navbar>
+    );
+  }
+}
+
+export class RollButton extends React.Component {
+  render() {
+    return (
+      <Button variant="warning" size="lg" style={{ width: '18rem'}}>Roll: -5</Button>
+    );
+  }
+}
+
+export class VentureCapitalButton extends React.Component {
+  render() {
+    return (
+      <Button variant="secondary" size="lg" style={{ width: '18rem'}}>Raise Venture Capital: +100</Button>
+    );
+  }
+}
+
+
+export class PlayerCard extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    let name = this.props.name; 
+    let rarity = this.props.rarity; 
+    let description = this.props.description;
+    let salary = this.props.salary;
+    let income = this.props.income;
+    let power = this.props.power;
+    let stars = ""
+    for (let i = 0; i < rarity; i++) {
+      stars += "★";
+    }
+    return (
+      <Card style={{ width: '18rem'}}>
+        <Card.Header as="h5">{stars}</Card.Header>
+        <Card.Img variant="top" src="/jowosie.jpg"/>
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            {description}
+          </Card.Text>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem><b>Power:</b>  {power}</ListGroupItem>
+            <ListGroupItem><b>Salary:</b> {salary}</ListGroupItem>
+            <ListGroupItem><b>Income:</b> {income}</ListGroupItem>
+          </ListGroup>
+        </Card.Body>
+      </Card>
+      );
+  }
+}
+
 export default App;
